e65e95f93dc7d9125ec9bc3b296f7995802d1c69
James Schumann <jschumann@gmail.com>
2017-01-16 09:17:58 -0500

    merged in code from old rest-api-usermeta plugin

--- plugin.php
+++ plugin.php
@@ -5,8 +5,9 @@
 * Description: Profile tweaks, custom meta fields, user roles, and API endpoints for Users.
 * Author: WRDSB
 * Author URI: https://github.com/wrdsb
-* Version: 0.0.3
+* Version: 1.0.0
 * License: GPLv3 or later
+* Text Domain: wrdsb-users
 * GitHub Plugin URI: wrdsb/wordpress-plugin-users
 * GitHub Branch: master
 */
