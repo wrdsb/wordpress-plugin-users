b2c244f721efcb85d2131fcc028eb8f889525bb8
James Schumann <jschumann@gmail.com>
2017-01-17 14:09:19 -0500

    Create register-routes.php

---
+++ rest-api/register-routes.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * WRDSB_REST_Users_Controller class.
+ */
+if ( ! class_exists( 'WRDSB_REST_Users_Controller' ) ) {
+	require_once dirname( __FILE__ ) . '/endpoints/class-wrdsb-rest-users-controller.php';
+}
