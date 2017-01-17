178bd6d6a7aef2a73a897ea546060b72f82796b7
James Schumann <jschumann@gmail.com>
2017-01-17 14:35:16 -0500

    Move each field into its own file

--- rest-api/register-fields.php
+++ rest-api/register-fields.php
@@ -1,94 +1,14 @@
 <?php
-# Add the field "wrdsb_id_number" to REST API responses for users read and write
-add_action( 'rest_api_init', 'usermeta_register_wrdsb_id_number' );
-function usermeta_register_wrdsb_id_number() {
-	register_rest_field( 'user',
-		'wrdsb_id_number',
-		array(
-			'get_callback'    => 'usermeta_get_value',
-			'update_callback' => 'usermeta_update_value',
-			'schema'          => null,
-		)
-	);
-}
-
-# Add the field "wrdsb_school" to REST API responses for users read and write
-add_action( 'rest_api_init', 'usermeta_register_wrdsb_school' );
-function usermeta_register_wrdsb_school() {
-	register_rest_field( 'user',
-		'wrdsb_school',
-		array(
-			'get_callback'    => 'usermeta_get_value',
-			'update_callback' => 'usermeta_update_value',
-			'schema'          => null,
-		)
-	);
-}
-
-# Add the field "wrdsb_voicemail" to REST API responses for users read and write
-add_action( 'rest_api_init', 'usermeta_register_wrdsb_voicemail' );
-function usermeta_register_wrdsb_voicemail() {
-	register_rest_field( 'user',
-		'wrdsb_voicemail',
-		array(
-			'get_callback'    => 'useroption_get_value',
-			'update_callback' => 'useroption_update_value',
-			'schema'          => null,
-		)
-	);
-}
-
-# Add the field "wrdsb_job_title" to REST API responses for users read and write
-add_action( 'rest_api_init', 'usermeta_register_wrdsb_job_title' );
-function usermeta_register_wrdsb_job_title() {
-	register_rest_field( 'user',
-		'wrdsb_job_title',
-		array(
-			'get_callback'    => 'useroption_get_value',
-			'update_callback' => 'useroption_update_value',
-			'schema'          => null,
-		)
-	);
-}
-
-# Add the field "wrdsb_display_in_staff_list" to REST API responses for users read and write
-add_action( 'rest_api_init', 'usermeta_register_wrdsb_display_in_staff_list' );
-function usermeta_register_wrdsb_display_in_staff_list() {
-	register_rest_field( 'user',
-		'wrdsb_display_in_staff_list',
-		array(
-			'get_callback'    => 'useroption_get_value',
-			'update_callback' => 'useroption_update_value',
-			'schema'          => null,
-		)
-	);
-}
-
-# Add the field "wrdsb_contact_options" to REST API responses for users read and write
-add_action( 'rest_api_init', 'usermeta_register_wrdsb_contact_options' );
-function usermeta_register_wrdsb_contact_options() {
-	register_rest_field( 'user',
-		'wrdsb_contact_options',
-		array(
-			'get_callback'    => 'useroption_get_value',
-			'update_callback' => 'useroption_update_value',
-			'schema'          => null,
-		)
-	);
-}
-
-# Add the field "wrdsb_website_url" to REST API responses for users read and write
-add_action( 'rest_api_init', 'usermeta_register_wrdsb_website_url' );
-function usermeta_register_wrdsb_website_url() {
-	register_rest_field( 'user',
-		'wrdsb_website_url',
-		array(
-			'get_callback'    => 'useroption_get_value',
-			'update_callback' => 'useroption_update_value',
-			'schema'          => null,
-		)
-	);
-}
+/**
+ * Require wrdsb_ Field Files
+ */
+require_once dirname( __FILE__ ) . '/fields/wrdsb-id-number.php';
+require_once dirname( __FILE__ ) . '/fields/wrdsb-school.php';
+require_once dirname( __FILE__ ) . '/fields/wrdsb-voicemail.php';
+require_once dirname( __FILE__ ) . '/fields/wrdsb-job-title.php';
+require_once dirname( __FILE__ ) . '/fields/wrdsb-display-in-staff-list.php';
+require_once dirname( __FILE__ ) . '/fields/wrdsb-contact-options.php';
+require_once dirname( __FILE__ ) . '/fields/wrdsb-website-url.php';
 
 /**
  * Handler for getting custom user meta field data.
