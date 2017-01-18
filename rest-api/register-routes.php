ca6dd826f99cedfe83dd8330b70ca198559e824f
James Schumann <jschumann@gmail.com>
2017-01-18 13:52:09 -0500

    added route wrdsb/v2/users-by-email

--- rest-api/register-routes.php
+++ rest-api/register-routes.php
@@ -5,3 +5,11 @@
 if ( ! class_exists( 'WRDSB_REST_Users_Controller' ) ) {
 	require_once dirname( __FILE__ ) . '/routes/class-wrdsb-rest-users-controller.php';
 }
+
+add_action( 'rest_api_init', 'create_wrdsb_rest_routes', 99 );
+function create_wrdsb_rest_routes() {
+	// Users.
+	$controller = new WRDSB_REST_Users_Controller;
+	$controller->register_routes();
+}
+
