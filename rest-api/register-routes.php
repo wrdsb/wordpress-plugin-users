43b9134204ce8c18771b8078a2de79062779838f
James Schumann <jschumann@gmail.com>
2017-04-19 07:45:00 -0400

    add rest routes for user-by-id-number and user-by-username

--- rest-api/register-routes.php
+++ rest-api/register-routes.php
@@ -4,12 +4,18 @@
  */
 if ( ! class_exists( 'WRDSB_REST_Users_Controller' ) ) {
 	require_once dirname( __FILE__ ) . '/routes/class-wrdsb-rest-user-by-email-controller.php';
+	require_once dirname( __FILE__ ) . '/routes/class-wrdsb-rest-user-by-username-controller.php';
+	require_once dirname( __FILE__ ) . '/routes/class-wrdsb-rest-user-by-id-number-controller.php';
 }
 
 add_action( 'rest_api_init', 'create_wrdsb_rest_routes', 99 );
 function create_wrdsb_rest_routes() {
 	// Users.
 	$controller = new WRDSB_REST_User_by_Email_Controller;
+	$controller->register_routes();
+	$controller = new WRDSB_REST_User_by_Username_Controller;
+	$controller->register_routes();
+	$controller = new WRDSB_REST_User_by_ID_Number_Controller;
 	$controller->register_routes();
 }
 
