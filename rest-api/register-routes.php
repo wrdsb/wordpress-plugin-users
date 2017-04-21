a221343dd51f11a47256f1fb8d5161f81eb2e310
James Schumann <jschumann@gmail.com>
2017-04-21 10:27:06 -0400

    now supports adding and removing users in subsites

--- rest-api/register-routes.php
+++ rest-api/register-routes.php
@@ -6,6 +6,7 @@ if ( ! class_exists( 'WRDSB_REST_Users_Controller' ) ) {
 	require_once dirname( __FILE__ ) . '/routes/class-wrdsb-rest-user-by-email-controller.php';
 	require_once dirname( __FILE__ ) . '/routes/class-wrdsb-rest-user-by-username-controller.php';
 	require_once dirname( __FILE__ ) . '/routes/class-wrdsb-rest-user-by-id-number-controller.php';
+	require_once dirname( __FILE__ ) . '/routes/class-wrdsb-rest-blog-user-controller.php';
 }
 
 add_action( 'rest_api_init', 'create_wrdsb_rest_routes', 99 );
@@ -16,6 +17,8 @@ function create_wrdsb_rest_routes() {
 	$controller = new WRDSB_REST_User_by_Username_Controller;
 	$controller->register_routes();
 	$controller = new WRDSB_REST_User_by_ID_Number_Controller;
+	$controller->register_routes();
+	$controller = new WRDSB_REST_Blog_User_Controller;
 	$controller->register_routes();
 }
 
