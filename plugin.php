361a532b8215640c932adaa4f8609a85f9b2377e
Suzanne <suzannezc@users.noreply.github.com>
2017-08-30 16:29:24 -0400

    version bump

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
 * Description: Profile tweaks, custom meta fields, user roles, and API endpoints for Users.
 * Author: WRDSB
 * Author URI: https://github.com/wrdsb
-* Version: 1.1.1
+* Version: 1.1.2
 * License: GPLv3 or later
 * Text Domain: wrdsb-users
 * GitHub Plugin URI: wrdsb/wordpress-plugin-users
