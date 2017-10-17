cd6cb01e6082dd9f97ec81a17cc43299c59a9ea4
James Schumann <jschumann@gmail.com>
2017-10-17 13:38:16 +0000

    fix register routes to use correct controller names

--- rest-api/register-routes.php
+++ rest-api/register-routes.php
@@ -20,11 +20,11 @@ function create_wrdsb_rest_routes() {
 	$controller->register_routes();
 	$controller = new WRDSB_REST_User_by_ID_Number_Controller;
 	$controller->register_routes();
-	$controller = new WRDSB_REST_Blog_by_Email_Controller;
+	$controller = new WRDSB_REST_Blog_User_by_Email_Controller;
 	$controller->register_routes();
-	$controller = new WRDSB_REST_Blog_by_Username_Controller;
+	$controller = new WRDSB_REST_Blog_User_by_Username_Controller;
 	$controller->register_routes();
-	$controller = new WRDSB_REST_Blog_by_ID_Number_Controller;
+	$controller = new WRDSB_REST_Blog_User_by_ID_Number_Controller;
 	$controller->register_routes();
 }
 
